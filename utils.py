48a5104df10e4d7e0365ec62d7fa2d018b3bb643
Quiet Soto <bylina.alexa2005@gmail.com>
2024-02-04 21:58:22 +0200

    second

---
+++ utils.py
@@ -0,0 +1,22 @@
+def prostota(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+        else:
+            pass
+    return True
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+def triangle(a, b, c):
+    if ((a+b)<c) or ((a+c)<b) or ((b+c)<a):
+        print ("Нема ніякого трикутника")
+    else:
+        if (a**2<b**2+c**2 or b**2<a**2+c**2 or c**2<b**2+a**2):
+            print ("Трикутник тупокутний")
+        else:
+            print ("Трикутник є, але НЕ тупокутний")
